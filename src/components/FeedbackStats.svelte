dce4de19b3ca3d3384e5ea8f07a76eda30363d9b
ornel77 <t.ornel@yahoo.fr>
2023-10-06 08:00:52 +0200

    first commit

---
+++ src/components/FeedbackStats.svelte
@@ -0,0 +1,17 @@
+<script>
+    export let count
+    export let average
+</script>
+
+<div class="feedback-stats">
+    <h4> {count} {count === 1 ? "review" : "reviews"} </h4>
+    <h4> Rating average:{average} </h4>
+</div>
+
+<style>
+    .feedback-stats {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+</style>
